0cbba97cbfdd8dc147adc9ccc897e187d63f06c4
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2024-08-14 19:46:29 -0400

    Single kerchunk debugging (with GitHub issue)

--- single-kerchunk.py
+++ single-kerchunk.py
@@ -1,7 +1,9 @@
+import json
+from pathlib import Path
+
 from kerchunk import hdf, df
 import fsspec.implementations.reference
 from fsspec.implementations.reference import LazyReferenceMapper
-import json
 
 print(fsspec.__version__)
 
@@ -21,12 +23,33 @@ out = LazyReferenceMapper.create(record_size=10_000, root="test.parq", fs=fs)
 out.flush()
 df.refs_to_dataframe(refs, "test.parq")
 
-# Test the read
+# Clean up parquet zmetadata
+with open("test.parq/.zmetadata", "r") as f:
+    zm = json.load(f)
 
+# Create a copy of the original metadata
+with open("test.parq/.zmetadata.orig", "w") as f:
+    f.write(json.dumps(zm, indent=2))
+
+# Remove keys that don't have any array information
+varnames = {k.split("/")[0] for k in zm["metadata"].keys() if "/" in k}
+haskeys = {x.name for x in Path("test.parq").glob("*/")}
+keepvars = varnames.difference(haskeys)
+zm2 = zm.copy()
+zm2_meta = zm2["metadata"]
+for key in list(zm2_meta.keys()):
+    for v in keepvars:
+        if v in key:
+            zm2_meta.pop(key)
+
+with open("test.parq/.zmetadata", "w") as f:
+    f.write(json.dumps(zm2))
+
+# Test the read
 import xarray as xr
 
 tjson = xr.open_dataset("test.json", engine="kerchunk")
-tparq = xr.open_dataset("test.parq", engine="kerchunk")
-
 tjson.Rad.mean().values
+
+tparq = xr.open_dataset("test.parq", engine="kerchunk")
 tparq.Rad.mean().values
