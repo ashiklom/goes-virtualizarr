dd23d9a94e3d7239e71ea050c47b866f9bbe8c71
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2024-08-14 10:11:22 -0400

    Clean up scripts to prepare for real kerchunking

--- create.py
+++ create.py
@@ -5,20 +5,12 @@ from virtualizarr import open_virtual_dataset
 import cftime
 import numpy as np
 
-# # All of these throw errors on open_virtual_dataset
-# dropvars = [
-#     "algorithm_product_version_container",
-#     "processing_parm_version_container",
-#     "goes_imager_projection",
-#     "algorithm_dynamic_input_data_container",
-#     "geospatial_lat_lon_extent",
-# ]
-
 def get_dt(ds):
     val = ds.t.values
     # Round to the nearest minute
     val_rnd = (val // (60)) * (60)
-    return cftime.num2date(val_rnd, ds.t.units)
+    cfdt = cftime.num2date(val_rnd, ds.t.units)
+    return np.array(cfdt, dtype="datetime64[ns]")
 
 def open_ds(fname, dropvars):
     try:
@@ -35,10 +27,19 @@ def open_ds(fname, dropvars):
 def virtualize_day_band(gday_path, band):
     gday_files = sorted(gday_path.glob(f"**/*M6C{band:02d}*.nc"))
 
-    # Figure out which variables to *drop* by loading all variables and dropping
-    # everything except radiance.
-    d0 = xr.open_dataset(gday_files[1])
+    # Figure out which variables to *drop* by loading all variables from first
+    # file and dropping everything except radiance.
+    d0 = xr.open_dataset(gday_files[0])
     dropvars = set(d0.keys()).difference({"Rad"})
+
+    # # All of these throw errors on open_virtual_dataset
+    # dropvars = [
+    #     "algorithm_product_version_container",
+    #     "processing_parm_version_container",
+    #     "goes_imager_projection",
+    #     "algorithm_dynamic_input_data_container",
+    #     "geospatial_lat_lon_extent",
+    # ]
 
     virtual_datasets = [open_ds(fname, dropvars) for fname in gday_files]
     virtual_datasets = [vd for vd in virtual_datasets if vd is not None]
@@ -56,22 +57,29 @@ def virtualize_day_band(gday_path, band):
     virtual_ds = virtual_ds.assign_coords({"time": dtimes})
     return virtual_ds
 
-# Try virtualizing one complete day for one bands
-basepath = Path("/css/geostationary/BackStage/")
-g16path = basepath / "GOES-16-ABI-L1B-FULLD"
-g17path = basepath / "GOES-17-ABI-L1B-FULLD"
+if __name__ == "__main__":
+    # Try virtualizing one complete day for one bands
+    basepath = Path("/css/geostationary/BackStage/")
+    g16path = basepath / "GOES-16-ABI-L1B-FULLD"
+    g17path = basepath / "GOES-17-ABI-L1B-FULLD"
 
-# List all the years
-g17_years = sorted(g17path.glob("????"))
-# List the days for the last available year
-g17_days = sorted(g17_years[-1].glob("*"))
+    # List all the years
+    # g17_years = sorted(g17path.glob("????"))
+    # List the days for the last available year
+    # g17_days = sorted(g17_years[-1].glob("*"))
 
-# First, let's try parsing all the files for 1 day and 1 band.
-gday = g17_days[0]
-gday_files = sorted(gday.glob("**/*M6C01*.nc"))
-gday_files = gday_files[0:5]
+    year = 2022
+    day = 1
+    band = 1
 
-gd1 = virtualize_day_band(g17_days[0], 1)
-dt_np = np.array(gd1.time.values, dtype="datetime64[ns]")
-gd1["time"] = xr.DataArray(dt_np, name="time", dims="time")
-gd1.virtualize.to_kerchunk("2022-001-combined.json", format="json")
+    print(f"Parsing files for {year=}, {day=}, {band=}")
+
+    gday = g17path / str(year) / f"{day:03d}"
+    if not gday.exists():
+        raise ValueError(f"{gday} not found.")
+
+    # First, let's try parsing all the files for 1 day and 1 band.
+    gd1 = virtualize_day_band(gday, 1)
+    outfile = Path("combined") / g17path.name / f"{year}-{day:03d}-B{band:02d}.json"
+    outfile.parent.mkdir(exist_ok=True, parents=True)
+    gd1.virtualize.to_kerchunk(outfile, format="json")
--- read.py
+++ read.py
@@ -1,6 +1,21 @@
 import xarray as xr
+import time
 
-combined_ds = xr.open_dataset("combined.parq", engine="kerchunk")
-# combined_ds = xr.open_dataset("combined.json", engine="kerchunk")
+def ttime(f):
+    ts = time.time()
+    result = f()
+    te = time.time()
+    print(f"Time: {te - ts:.02f} sec")
+    return result
 
-print(combined_ds.Rad.mean().values)
+# combined_ds = xr.open_dataset("combined.parq", engine="kerchunk")
+print("Opening file")
+combined_ds = ttime(lambda: xr.open_dataset(
+    "combined/GOES-17-ABI-L1B-FULLD/2022-001-B01.json",
+    engine="kerchunk"
+))
+
+print("Calculating global mean")
+result = ttime(lambda: combined_ds.Rad.mean().values)
+
+print(f"Global mean: {result}")
